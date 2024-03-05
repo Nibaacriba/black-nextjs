74468a860ba5e19db73a46399b3af35635874f6a
@gabriel.barbaceli <gabriel.barbaceli@gmail.com>
2024-03-05 14:36:20 -0300

    fix: apis resp

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import products from "../../../database.json";
+import Products from "../../../database.json";
+
 export default function handler(req: NextApiRequest, resp: NextApiResponse) {
-  const { id } = req.query;
-
-  const product = products.find((prod) => prod.id === Number(id));
-
-  resp.status(200).json(product);
+  resp.status(200).json(Products);
 }
--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -1,6 +1,10 @@
 import Products from "@/pages/products";
 import { NextApiRequest, NextApiResponse } from "next";
+import products from "../../../../database.json";
 
 export default function handler(req: NextApiRequest, resp: NextApiResponse) {
-  resp.status(200).json(Products);
+  const { id } = req.query;
+
+  const product = products.find((prod) => prod.id === Number(id));
+  resp.status(200).json(product);
 }
