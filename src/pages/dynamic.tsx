036eaf2edb0837637e89ed31354b9adfcc2eb2ad
@gabriel.barbaceli <gabriel.barbaceli@gmail.com>
2024-03-05 10:23:25 -0300

    feat: added SSR component

---
+++ src/pages/dynamic.tsx
@@ -0,0 +1,54 @@
+import { GetServerSideProps, NextPage } from "next";
+import { ReactNode, useEffect, useState } from "react";
+import { Col, Container, Row } from "reactstrap";
+
+interface ApiResponse {
+  name: string;
+  timeStamp: Date;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideData: ApiResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_APIURL}/api/hello`
+  ).then((resp) => resp.json());
+  return {
+    props: {
+      serverSideData,
+    },
+  };
+};
+
+const Dynamic: NextPage = (props: {
+  children?: ReactNode;
+  serverSideData?: ApiResponse;
+}) => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>();
+
+  const fetchData = async () => {
+    const data = await fetch("/api/hello").then((resp) => resp.json());
+    setClientSideData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <Container tag="main">
+      <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
+
+      <Row>
+        <Col>
+          <h3>
+            Gerado no servidor: {props?.serverSideData?.timeStamp.toString()}
+          </h3>
+        </Col>
+
+        <Col>
+          <h3>Gerado no cliente: {clientSideData?.timeStamp.toString()}</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Dynamic;
